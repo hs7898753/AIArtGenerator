d3d729a33f5f039ca415b97cda353d6f46ebbd6a
hs7898753 <hs7898753@gmail.com>
2023-12-17 22:51:40 +0530

    file structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+
+export default function DashboardPage() {
+  return (
+    <div>
+        Dashboard Page
+      <Button>Button</Button>
+    </div>
+  )
+}
