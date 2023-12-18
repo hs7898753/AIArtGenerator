2cf6b73df24ab41a5571f72327649fda62c11aa2
hs7898753 <hs7898753@gmail.com>
2023-12-18 14:43:18 +0530

    Navbar and Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 
 export default function DashboardPage() {
   return (
     <div>
         Dashboard Page
-      <Button>Button</Button>
+      
     </div>
   )
 }
